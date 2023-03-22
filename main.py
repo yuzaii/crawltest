49d2ded397e2913608736e0419007ed5588b644b
yuzai_win <yuzaii@qq.com>
2023-03-23 00:42:30 +0800

    再修改看你能不能发现

--- main.py
+++ main.py
@@ -14,11 +14,11 @@ def start():
     for i in range(10):
         # res = requests.get('http://127.0.0.1:6060/post/postcategory')
         # print(res.text)
-        logging.debug('修改This is a debug message')
-        logging.info('修改This is an info message')
-        logging.warning('修改This is a warning message')
-        logging.error('修改This is an error message')
-        logging.critical('修改This is a critical message')
+        logging.debug('修改This is a debug message修改1')
+        logging.info('修改This is an info message修改1')
+        logging.warning('修改This is a warning message修改1')
+        logging.error('修改This is an error message修改1')
+        logging.critical('修改This is a critical message修改1')
         time.sleep(3)
 
 
