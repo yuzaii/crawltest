9f8a0f211fcf2ee44224680096736e38b96cbd13
yuzai_win <yuzaii@qq.com>
2023-03-23 00:41:11 +0800

    看你能不能发现

--- main.py
+++ main.py
@@ -14,11 +14,11 @@ def start():
     for i in range(10):
         # res = requests.get('http://127.0.0.1:6060/post/postcategory')
         # print(res.text)
-        logging.debug('This is a debug message')
-        logging.info('This is an info message')
-        logging.warning('This is a warning message')
-        logging.error('This is an error message')
-        logging.critical('This is a critical message')
+        logging.debug('修改This is a debug message')
+        logging.info('修改This is an info message')
+        logging.warning('修改This is a warning message')
+        logging.error('修改This is an error message')
+        logging.critical('修改This is a critical message')
         time.sleep(3)
 
 
